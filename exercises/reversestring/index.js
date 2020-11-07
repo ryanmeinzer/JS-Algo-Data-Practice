7a167e5676eb9024338cba378265b7e993d8e99a
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-06 17:44:21 -0800

    played around with debugger

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -11,16 +11,20 @@ function reverse(str) {
     // return str.split('').reverse().join('')
 
     // // solve with for loop 
-    // let reversed = ''
-    // // for (let i = 0; i < str.length; i++){
-    // //     reversed = str[i] + reversed
-    // for (let character of str){
-    //     reversed = character + reversed
-    // }
-    // return reversed
+    let reversed = ''
+    // for (let i = 0; i < str.length; i++){
+    //     reversed = str[i] + reversed
+    for (let character of str){
+        reversed = character + reversed
+        // debugger
+    }
+    return reversed
 
     // solved with reduce 
-        return str.split('').reduce((reversed, character) => character + reversed, '')
-    }
+    //     return str.split('').reduce((reversed, character) => character + reversed, '')
+
+}
+
+// reverse('apple')
 
 module.exports = reverse;
