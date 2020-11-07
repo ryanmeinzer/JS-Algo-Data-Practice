f8db203c21fb21a598f28e14ad17a49957ad7450
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-06 17:21:53 -0800

    added solution with reduce

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -10,14 +10,17 @@ function reverse(str) {
     // solved with reverse() 
     // return str.split('').reverse().join('')
 
-    // solve with for loop 
-    let reversed = ''
-    // for (let i = 0; i < str.length; i++){
-    //     reversed = str[i] + reversed
-    for (let character of str){
-        reversed = character + reversed
+    // // solve with for loop 
+    // let reversed = ''
+    // // for (let i = 0; i < str.length; i++){
+    // //     reversed = str[i] + reversed
+    // for (let character of str){
+    //     reversed = character + reversed
+    // }
+    // return reversed
+
+    // solved with reduce 
+        return str.split('').reduce((reversed, character) => character + reversed, '')
     }
-    return reversed
-}
 
 module.exports = reverse;
