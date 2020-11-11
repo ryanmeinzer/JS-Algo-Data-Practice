5867a0b0b7d43e7d860dc871eff86d200a5eb5bc
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-10 16:01:13 -0800

    solved chunk with USSR method

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,7 +9,27 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    
+    // USSR
+    // Understand - given size, return as many arrays as possible of that size. Inputs are integers, outputs are integers. Labels are present. Example is if size is 3 and array size is 10, four arrays returned with three being of size three and the last being of size 1
+    // Sudo chop - 
+    // Solve / skip - 
+    // create new array for chunks (chunked)
+    let chunked = []
+    // for each element in array, get last element in chunked
+    for (let element of array) {
+        let last = chunked[chunked.length - 1]
+        // if last element doesn't exist or if its length is equal to size
+        if (!last || last.length === size) {
+            // push a new[ chunk into chunked with the current element
+            chunked.push([element])
+            // else add the current element into current chunk
+        } else {
+            last.push(element)
+        }
+    }
+    // return chunked
+    return chunked 
+    // Refactor - 
 }
 
 module.exports = chunk;
