56af0425c8380f93148dcc1774190ec5ec1d140b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-10 22:11:39 -0800

    solved capitalize with split looped upper case slice and join

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,46 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    // My USSR Approach
+    // Understand
+        // reword - return each word of a string capitalized
+        // inputs - string, characters
+        // outputs - string
+        // examples - capitalize('here's An example) --> 'Here's An Example'
+    // Sudo Chop
+    // Solve / Skip
+        // // <-- START UNCOMMENT
+        // // split each word of string by space into an array
+        // let splitString = str.split(' ')
+        // // create capitalized array
+        // let capitalizedArray = []
+        // // loop over array
+        // for (let word of splitString) {
+        //     // capitalize the first letter of each word of the array
+        //     word = word[0].toUpperCase() + word.slice(1)
+        //     // push each word into capitalizedArray
+        //     capitalizedArray.push(word)
+        // }
+        // // join capitalized array back into a string
+        // let joinedString = capitalizedArray.join(' ')
+        // // return joined string
+        // return joinedString
+        // // END UNCOMMENT -->
+    
+    // Refactor
+    // create capitalized array
+    let capitalizedArray = []
+    // loop over array
+    // split each word of string by space into an array
+    for (let word of str.split(' ')) {
+        // capitalize the first letter of each word of the array
+        // push each word into capitalizedArray
+        capitalizedArray.push(word[0].toUpperCase() + word.slice(1))
+    }
+    // join capitalized array back into a string
+    // return joined string
+    return capitalizedArray.join(' ')
+}
 
 module.exports = capitalize;
