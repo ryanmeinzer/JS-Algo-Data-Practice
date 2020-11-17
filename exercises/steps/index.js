1e13f0019c749f286207682cb364787fc2b0ea47
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-17 14:41:07 -0800

    solved steps with recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,38 +17,7 @@
 //       '### '
 //       '####'
 
-// solved with nested loops
-function steps(n) {
-    // USSR
-    // Understand
-    // rephrase - make n stairs deep/lines with n stairs wide
-    // input - integer
-    // output - strings
-    // examples - steps(2)
-    // '# '
-    // '##'
-    // Sudo Chop
-    // Solve / Skip
-    // loop through rows from 0 to n
-    for (let row = 0; row < n; row++) {
-    // create an empty string 'stair'
-        let stair = ''
-    // loop through columns from 0 to n
-        for (let column = 0; column < n; column++) {
-    // if the current column is <= current row, add a # to 'stair'
-            if (column <= row) {
-                stair += '#'
-    // else add a space to 'stair'
-            } else {
-                stair += ' '
-            } 
-        }
-    // console log 'stair'
-        console.log(stair)
-    // Refactor 
-    }
-}
-
+// solved with interpolation
 // function steps(n) {
 //     // USSR
 //     // Understand
@@ -71,7 +40,7 @@ function steps(n) {
 //             // create n - step ' ' strings
 //             let spaceStr = ' '
 //             if (n > step + 1) {
-                
+
 //                 spaceStr = spaceStr.repeat(n - step - 1)
 //             }
 //             // console log each iteration interpolated with variables
@@ -79,5 +48,63 @@ function steps(n) {
 //         }
 //     // Refactor
 // }
+
+// // solved with iterative nested loops
+// function steps(n) {
+//     // USSR
+//     // Understand
+//     // rephrase - make n stairs deep/lines with n stairs wide
+//     // input - integer
+//     // output - strings
+//     // examples - steps(2)
+//     // '# '
+//     // '##'
+//     // Sudo Chop
+//     // Solve / Skip
+//     // loop through rows from 0 to n
+//     for (let row = 0; row < n; row++) {
+//     // create an empty string 'stair'
+//         let stair = ''
+//     // loop through columns from 0 to n
+//         for (let column = 0; column < n; column++) {
+//     // if the current column is <= current row, add a # to 'stair'
+//             if (column <= row) {
+//                 stair += '#'
+//     // else add a space to 'stair'
+//             } else {
+//                 stair += ' '
+//             } 
+//         }
+//     // console log 'stair'
+//         console.log(stair)
+//     // Refactor 
+//     }
+// }
+
+// solved with recursion
+function steps(n, row = 0, stair = '', increment = 1) {
+    // make a base case
+    if (row === n){
+        return 
+    }
+    // create an empty string 'stair' (in parameters)
+    // if the 'stair' string's length is equal to n, we're at the end of the row
+    if (stair.length === n) {
+        console.log(stair)
+    // change function to increment accordingly to base case
+        steps(n, row + increment)
+    // make increment default (in parameters)
+        return
+    }   
+    // if the 'stair' string's length is <= the row, add '#'
+    if (stair.length <= row) {
+        stair += '#'
+    // else add ' '
+    } else {
+        stair += ' '
+    }
+    steps(n, row, stair)
+
+}
 
 module.exports = steps;
