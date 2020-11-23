5d44d732b51246482733208b84f60d41317a64f4
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-22 18:17:31 -0800

    solved stack with class

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,29 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+// USSR
+// Understand
+// Sudo Chop
+    // create constructor
+    constructor() {
+        // create 'data' array
+        this.data = []
+    }
+    // create 'push' method with push
+    push(record) {
+        return this.data.push(record)
+    }
+    // create 'pop' method with pop
+    pop() {
+        return this.data.pop()
+    }
+    // create 'peek' method
+    peek() {
+        return this.data[this.data.length - 1]
+    }
+// Solve / Chop
+// Refactor
+}
 
 module.exports = Stack;
