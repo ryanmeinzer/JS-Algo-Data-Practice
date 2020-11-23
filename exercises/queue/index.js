f82e974b5d5d17851e4c1f8a5ef0ddcde87cc762
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-22 16:20:30 -0800

    solved queue with a class queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,27 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+// USSR
+// Understand
+    // create a class that instantiates a FIFO queue with methods 'add' and 'remove'
+// Sudo Chop
+    // create constructor
+    constructor() {
+        // create 'data' array for queue class
+        this.data = []
+    }
+    // create 'add' and 'remove' method for unshift() and pop() for a 'record' parameter
+    add(record) {
+        this.data.unshift(record)
+    }
+    remove() {
+        return this.data.pop()
+    }
+// Solve / Skip
+// Refactor
+
+
+}
 
 module.exports = Queue;
