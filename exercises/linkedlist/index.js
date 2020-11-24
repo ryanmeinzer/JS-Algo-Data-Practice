9e9f691d2496a438db8305961a7f4832f7a5e84a
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-23 17:38:04 -0800

    began solving problems for linkedlist class after creating node class

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,29 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+
+    constructor() {
+        this.head = null
+    }
+
+    insertFirst(data) {
+        // pass in the existing this.head as the second argument to assign to the new head node's next argument to link them
+        // update this.head to be the new head node
+        this.head = new Node(data, this.head)
+    }
+
+    size() {
+        
+    }
+    
+}
 
 module.exports = { Node, LinkedList };
