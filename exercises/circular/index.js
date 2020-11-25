08cc1479c96ea0bf070cb83a128d08652a47feea
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-24 18:48:14 -0800

    solved linkedlist circular loop tester with while loop and pointer evaluations

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,29 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+
+    // Sudo Chop
+    // create two variables 'slow' and 'fast'
+    // assign both variables to first/head node
+    let slow = list.getFirst()
+    // or: let slow = list.head
+    let fast = list.getFirst()
+    // iterate through linkedlist with a while loop
+    // determine if fast.next exists and fast.next.next exists
+    while (fast.next && fast.next.next) {
+        // increment slow by one and fast by two
+        slow = slow.next
+        fast = fast.next.next
+        // if slow and fast are looking/pointing at the same node, it's a circular linked list
+        if (slow === fast) {
+            // return true
+            return true
+        }
+    }
+    // if we exit the loop, that means the end node pointed to null and it's not a circular linked list
+    return false
+
+}
 
 module.exports = circular;
