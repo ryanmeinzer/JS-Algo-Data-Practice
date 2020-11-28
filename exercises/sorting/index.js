8a8e86acd262a111d83d3c8e89961092d5ac6307
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-11-27 16:23:39 -0800

    solved merge sort with accompanying merge() function and recursion

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -47,13 +47,26 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-
+    // make sure the array has more than one element in it to split
+    if (arr.length === 1) {
+        return arr
+    }
+    // determine center of array
+    let center = Math.floor(arr.length / 2)
+    // split the array into two then split those halves by using slice starting with the first index and slicing up to (but not including) the center
+    let left = arr.slice(0, center)
+    let right = arr.slice(center)
+    // recursively call mergeSort() into each half
+    // mergeSort(left)
+    // mergeSort(right)
+    // recursively call mergeSort() into each half AND recursively call merge() on both left and right single-element arrays, and if a single-element array (sorted) only has one value, it's returned for the respective value in merge() per the first if statement in mergeSort()
+    return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
-    // create an empty 'results' array for two merged arrays
+    // create an empty 'results' array to merge two sorted arrays
     let results = []
-    // while there are still elements in both 'left' and 'right' arrays
+    // while there are still elements in both the sorted 'left' and sorted 'right' arrays
     while (left.length && right.length) {
         // if the first element of the left-half array is less than the first element of the right-half array
         if (left[0] < right[0]) {
