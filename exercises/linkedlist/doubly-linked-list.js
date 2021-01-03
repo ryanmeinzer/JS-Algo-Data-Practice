694abb80d9c2f5f8be115a10bce7176ff33763fa
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-02 17:30:47 -0800

    added get method

--- exercises/linkedlist/doubly-linked-list.js
+++ exercises/linkedlist/doubly-linked-list.js
@@ -73,7 +73,27 @@ class DoublyLinkedList {
         this.length++
         return this
     }
-
+    get(index) {
+        if (index < 0 || index >= this.length) return null
+        let count = 0
+        let current
+        if (index <= this.length / 2) {
+            count = 0
+            current = this.head
+            while (count !== index) {
+                current = current.next
+                count++
+            }
+        } else {
+            count = this.length - 1
+            current = this.tail
+            while (count !== index) {
+                current = current.prev
+                count--
+            }
+        }
+        return current
+    }
 
 }
 let list = new DoublyLinkedList()
