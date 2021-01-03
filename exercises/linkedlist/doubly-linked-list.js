a837018374f54da2f94c47546042777e096df23f
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-02 17:27:20 -0800

    added unshift method

--- exercises/linkedlist/doubly-linked-list.js
+++ exercises/linkedlist/doubly-linked-list.js
@@ -60,6 +60,20 @@ class DoublyLinkedList {
         this.length--
         return oldHead
     }
+    unshift(value) {
+        let newNode = new Node(value)
+        if (this.length === 0) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+
 
 }
 let list = new DoublyLinkedList()
