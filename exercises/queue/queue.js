7950be8437f9c08a524a87b3f5a2c640efebb57c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-02 19:01:27 -0800

    added queue with array and with singly linked list along with big o notations

---
+++ exercises/queue/queue.js
@@ -0,0 +1,47 @@
+// easy way to create a FIFO queue with an array but *not* with constant time 0(1):
+let queue = []
+add(record) {
+    queue.unshift(record)
+}
+remove() {
+    queue.pop()
+}
+
+// creating FIFO queue from scratch with a singly linked list with constant time 0(1) for insertion and removal
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val) {
+        var newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if (!this.first) return null;
+
+        var temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
