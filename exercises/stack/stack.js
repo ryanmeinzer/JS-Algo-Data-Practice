54a6779a96a813f4d44dea2926f72473368d14cd
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-02 18:34:25 -0800

    added stack with push and pop methods with time complexities

---
+++ exercises/stack/stack.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+    // 0(1) because we're adding from the beginning
+    push(val) {
+        let newNode = new Node(val)
+        if (!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let temp = this.first
+            this.first = newNode
+            this.first.next = temp
+        }
+        return ++this.size
+        // same as:
+        // this.size++
+        // return this.size
+    }
+    // 0(1) because we're removing from the beginning
+    pop() {
+        if (!this.first) return null
+        let temp = this.first
+        if (this.first === this.last) {
+            this.last = null
+        }
+        this.first = this.first.next
+        this.size--
+        return temp.value
+    }
+}
+
+let stack = new Stack()
+stack.push("HELLO")
+stack.push("GOODBYE")
+stack.push("!")
+
