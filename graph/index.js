7ee280f76899af662bd758ac12c21d9b0ccb66e3
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-01-03 22:27:04 -0800

    added DFRecursive method

--- graph/index.js
+++ graph/index.js
@@ -32,18 +32,73 @@ class Graph {
         // find the key of vertex2 and push vertex1 to the array
         this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
     }
+    // accept a starting node
+    DFRecursive(start) {
+        // create 'result' array to store the end result
+        const result = []
+        // create 'visited' object to store all vertexes visited
+        const visited = {}
+        // preserve context of adjacencyList for helper function
+        const adjacencyList = this.adjacencyList
+        // create a DFS helper function that accepts a vertex
+        function DFS(vertex) {
+            // if the vertex is empty, return null
+            if (!vertex) return null
+            // place vertex into visited object
+            visited[vertex] = true
+            // push vertex into result array
+            result.push(vertex)
+            // loop over all values in adjacencyList for that vertex
+            adjacencyList[vertex].forEach(neighbor => {
+                // if any values haven't been visited, recursively invoke the helper function with that vertex
+                if (!visited[neighbor]) {
+                    return DFS(neighbor)
+                }
+            })
+        }
+        // invoke DFS helper function
+        DFS(start)
+        return result
+    }
 }
 
-let g = new Graph()
-g.addVertex("Dallas")
-g.addVertex("Tokyo")
-g.addVertex("Aspen")
-g.addVertex("Los Angeles");
-g.addVertex("Hong Kong")
-g.addEdge("Dallas", "Tokyo");
-g.addEdge("Dallas", "Aspen");
-g.addEdge("Hong Kong", "Tokyo");
-g.addEdge("Hong Kong", "Dallas");
-g.addEdge("Los Angeles", "Hong Kong");
-g.addEdge("Los Angeles", "Aspen");
+let g = new Graph();
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+g.DFRecursive("A")
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
+
+// let g = new Graph()
+// g.addVertex("Dallas")
+// g.addVertex("Tokyo")
+// g.addVertex("Aspen")
+// g.addVertex("Los Angeles")
+// g.addVertex("Hong Kong")
+// g.addEdge("Dallas", "Tokyo")
+// g.addEdge("Dallas", "Aspen")
+// g.addEdge("Hong Kong", "Tokyo")
+// g.addEdge("Hong Kong", "Dallas")
+// g.addEdge("Los Angeles", "Hong Kong")
+// g.addEdge("Los Angeles", "Aspen")
 // breakpoint
